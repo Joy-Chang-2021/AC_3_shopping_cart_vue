f0590582cb55e62c79a0c239d3a92d823f917472
Joy-Chang-2021 <joy50422@gmail.com>
2022-02-07 15:13:08 +0800

    (1) StepsPanel.vue / template (2) organize scss & css file

--- src/components/StepsPanel.vue
+++ src/components/StepsPanel.vue
@@ -1,3 +1,18 @@
 <template>
-  <h3>Steps Panel</h3>
+  <div class="d-flex justify-content-between align-items-center">
+    <div class="step">
+      <h4>1</h4>
+      <h5>寄送地址</h5>
+    </div>
+    <span class="line"></span>
+    <div class="step">
+      <h4>2</h4>
+      <h5>運送方式</h5>
+    </div>
+    <span class="line"></span>
+    <div class="step">
+      <h4>3</h4>
+      <h5>付款資料</h5>
+    </div>
+  </div>
 </template>
